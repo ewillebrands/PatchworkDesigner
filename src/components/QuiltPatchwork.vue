2b4e40705db77e64b1a3c039d1e28d83061c6554
ewillebrands <e.a.t.willebrands@gmail.com>
2025-08-13 22:30:04 +0200

    basic patchwork builder

--- src/components/QuiltPatchwork.vue
+++ src/components/QuiltPatchwork.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
 import QuiltBlock from './QuiltBlock.vue'
-import { ref } from 'vue'
+import type { quilt } from './_types'
 
-const quilt = ref([{ name: 'block 1' }, { name: 'block 2' }])
+const quilt: quilt = {
+  blockCountWidth: 4,
+  blockCountLength: 6,
+  blocks: [
+    { name: 'block 1' },
+    { name: 'block 2' },
+    { name: 'block 3' },
+    { name: 'block 4' },
+    { name: 'block 5' },
+  ],
+}
 </script>
 
 <template>
-  <QuiltBlock v-for="blocks in quilt" />
+  <div
+    class="patchwork"
+    :style="{
+      'grid-template-columns': `repeat(${quilt.blockCountWidth}, 1fr)`,
+      'grid-template-rows': `repeat(${quilt.blockCountLength}, 1fr)`,
+    }"
+  >
+    <QuiltBlock v-for="block in quilt.blocks" :key="block.name" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.patchwork {
+  display: grid;
+  /* grid-template-columns: repeat(4, 1fr); */
+  /* grid-template-rows: repeat(6, 1fr); */
+  background-color: blue;
+}
+</style>
---
+++ src/components/_types.ts
@@ -0,0 +1,8 @@
+export interface quilt {
+  blockCountWidth: number
+  blockCountLength: number
+  blocks: block[]
+}
+export interface block {
+  name: string
+}
