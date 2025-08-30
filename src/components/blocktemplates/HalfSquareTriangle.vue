cab43dee74eb40ef044bc7defcea52d8e20c6e2a
ewillebrands <e.a.t.willebrands@gmail.com>
2025-08-30 16:58:47 +0200

    fix prop error

--- src/components/blocktemplates/HalfSquareTriangle.vue
+++ src/components/blocktemplates/HalfSquareTriangle.vue
@@ -10,8 +10,8 @@ const props = withDefaults(
 </script>
 
 <template>
-  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges">
-    <path d="M 0 0 L 100 100 H 0 Z" :fill="fabrics[0]" />
-    <path d="M 0 0 L 100 100 V 0 Z" :fill="fabrics[1]" />
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M 0 0 L 100 100 H 0 Z" :fill="props.fabrics[0]" />
+    <path d="M 0 0 L 100 100 V 0 Z" :fill="props.fabrics[1]" />
   </svg>
 </template>
