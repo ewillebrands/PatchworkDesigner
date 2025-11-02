e24b2dcd05278fb50124358c3a3692572f5c944c
ewillebrands <e.a.t.willebrands@gmail.com>
2025-11-02 18:44:52 +0100

    get fabrics for plainpatch from DB

--- src/components/blocktemplates/PlainPatch.vue
+++ src/components/blocktemplates/PlainPatch.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import QuiltprojectService from '@/services/QuiltprojectService'
+import type { fabric } from '../_types'
+import { ref } from 'vue'
+
 const props = withDefaults(
   defineProps<{
     fabrics?: string[]
   }>(),
   {
-    fabrics: () => ['#22aa22'],
+    fabrics: () => ['dawn-light'],
   },
 )
+
+const fabricsData = ref<fabric[]>([])
+
+onMounted(async () => {
+  console.log('Fetching fabrics for:', props.fabrics)
+  for (const fabric of props.fabrics) {
+    console.log('Fabric:', fabric)
+    const fabricData = await QuiltprojectService.getFabricByName(fabric)
+    if (fabricData.data) {
+      fabricsData.value.push(fabricData.data)
+    } else {
+      console.warn(`Fabric not found: ${fabric}`)
+    }
+  }
+})
 </script>
 
 <template>
-  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <path d="M 0 0 H 100 V 100 H 0 Z" :fill="props.fabrics[0]" />
+  <svg v-if="fabricsData.length >= 1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <path d="M 0 0 H 100 V 100 H 0 Z" :fill="fabricsData[0].color" />
   </svg>
 </template>
 
