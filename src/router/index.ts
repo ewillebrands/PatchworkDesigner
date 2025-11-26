694747232847b5e507af52e9f681e814a8cd35fc
ewillebrands <e.a.t.willebrands@gmail.com>
2025-11-26 17:52:34 +0100

    clearer names for components and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
-import QuiltDesignView from '../views/QuiltDesignView.vue'
-import BlockDesignView from '../views/BlockDesignView.vue'
+import QuiltDesignEditor from '../views/QuiltDesignEditor.vue'
+import BlockDesignEditor from '../views/BlockDesignEditor.vue'
 import NotFound from '../views/NotFound.vue'
 import NetworkError from '@/views/NetworkError.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/quiltdesign', name: 'quiltdesign', component: QuiltDesignView },
-  { path: '/blockdesign/:name', name: 'blockdesign', component: BlockDesignView, props: true },
+  { path: '/quiltdesign', name: 'quiltdesign', component: QuiltDesignEditor },
+  { path: '/blockdesign/:name', name: 'blockdesign', component: BlockDesignEditor, props: true },
   { path: '/network-error', name: 'networkerror', component: NetworkError },
   { path: '/404/:resource', name: 'notfoundwithresource', component: NotFound, props: true },
   { path: '/:catchAll(.*)', name: 'notfound', component: NotFound },
