d9ec6ac7593043e48995fe851bbb5bb90ff333f8
ewillebrands <e.a.t.willebrands@gmail.com>
2025-12-20 20:27:22 +0100

    fix for selection z-index

--- src/components/QuiltBlock.vue
+++ src/components/QuiltBlock.vue
@@ -49,19 +49,20 @@ const unselectBlock = () => {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
+  z-index: 0;
 
   &:hover {
     border: 1px solid var(--color-background);
     border-radius: 2px;
     cursor: pointer;
     position: relative;
-    z-index: 50;
+    z-index: 1;
   }
   &:active {
     border: 4px solid var(--color-primary);
     border-radius: 2px;
     position: relative;
-    z-index: 60;
+    z-index: 1;
   }
 }
 
@@ -71,6 +72,6 @@ const unselectBlock = () => {
   outline-style: dotted;
   border: none;
   border-radius: 2px;
-  z-index: 100;
+  z-index: 2;
 }
 </style>
